b01aa729740aa80e8656a42157795953eff21f68
rafaelreisramos <rrramos@gmail.com>
2022-06-10 16:23:04 -0300

    fix: replace assert.ok with assert.equal

--- tests/unit/factories/heroFactory.test.js
+++ tests/unit/factories/heroFactory.test.js
@@ -4,10 +4,10 @@ import { generateInstance } from '../../../src/factories/heroFactory.js'
 import HeroService from '../../../src/services/heroService.js'
 
 test('Hero factories - factories test suite', async (t) => {
-  await t.test('it should call generate a hero instance', () => {
+  await t.test('it should generate a heroService instance', () => {
     const generatedInstance = generateInstance({
       filePath: 'filePath',
     })
-    assert.ok(true, generatedInstance instanceof HeroService)
+    assert.equal(generatedInstance instanceof HeroService, true)
   })
 })
